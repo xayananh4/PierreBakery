48b0ca88face9279f16b1266b4e0b4568116bb2b
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-02-26 18:11:46 -0800

    remove loaf properties from bread class and calculate total method

--- PierreBakery/Models/Bread.cs
+++ PierreBakery/Models/Bread.cs
@@ -2,22 +2,16 @@ namespace PierreBakery.Models
 {
   public class Bread : Dough
   {
-    // private int _loaf;
-    // public int Loaf
-    // {
-    //   get { return _loaf; }
-
-    // }
     public Bread(int quantity)
     {
       Quantity = quantity;
-
     }
 
-    public int CalculateBreadTotal(int numOfLoaf)
+    //implement caltotal method from dough class
+    public override int CalTotal(int Quantity)
     {
       int totalPrice = 0;
-      for (int i = 0; i <= numOfLoaf; i++)
+      for (int i = 0; i <= Quantity; i++)
       {
         if (i % 3 == 0)
         {
@@ -30,28 +24,5 @@ namespace PierreBakery.Models
       }
       return totalPrice;
     }
-
-       //implement caltotal method from dough class
-   public override int CalTotal(int Quantity)
-   {
-     int totalPrice = 0;
-     for (int i = 0; i <= Quantity; i++)
-     {
-       if (i % 3 == 0)
-       {
-         totalPrice += 0;
-       }
-       else
-       {
-         totalPrice += 5;
-       }
-     }
-    return totalPrice;
-   }
-
-
-
-
-
   }
 }

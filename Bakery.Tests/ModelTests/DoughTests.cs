78dcf70b3b794131eaf81d6ef921b250c8a68bec
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-02-26 16:33:36 -0800

    turn dough class into abstract class with abstract members, property and method

--- Bakery.Tests/ModelTests/DoughTests.cs
+++ Bakery.Tests/ModelTests/DoughTests.cs
@@ -7,12 +7,13 @@ namespace Bakery.Tests
   [TestClass]
   public class DoughTests
   {
-    [TestMethod]
-    public void DoughConstructor_CreatesInstanceOfDough_Dough()
-    {
-      Dough newDough = new Dough();
-      Assert.AreEqual(typeof(Dough), newDough.GetType());
-    }
+    // [TestMethod]
+    // public void DoughConstructor_CreatesInstanceOfDough_Dough()
+    // {
+    //   Dough newDough = new Dough();
+    //   Assert.AreEqual(typeof(Dough), newDough.GetType());
+    // }
+  
   }
 
 }

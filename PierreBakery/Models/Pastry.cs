de7087c5c88f5f940d68623a0a24f574bd039dfd
Viradeth Xay-Ananh <xayananh4@gmail.com>
2023-02-26 18:13:25 -0800

    refactor Pastry class, remove pastries property and calculate total method

--- PierreBakery/Models/Pastry.cs
+++ PierreBakery/Models/Pastry.cs
@@ -2,35 +2,10 @@ namespace PierreBakery.Models
 {
   public class Pastry : Dough
   {
-    // private int _pastry;
-    // public int Pastries
-    // {
-    //   get { return _pastry; }
-    // }
-
     public Pastry(int quantity)
     {
       Quantity = quantity;
     }
-
-    public int CalculatePastryTotal(int numOfPastries)
-    {
-      int totalPrice = 0;
-      int noChargeLoaf = 0;
-      for (int i = 0; i <= numOfPastries; i++)
-      {
-        if (i % 4 == 0)
-        {
-          totalPrice = totalPrice + noChargeLoaf;
-        }
-        else
-        {
-          totalPrice = totalPrice + 2;
-        }
-      }
-      return totalPrice;
-    }
-
     public override int CalTotal(int Quantity)
     {
       int totalPrice = 0;
